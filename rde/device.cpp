a6c1191ac2c7851b0af37ac62418d8efece281dc
Jayanth Othayoth <Jayanth.Othayoth@amd.com>
2025-07-04 04:47:20 +0000

    rde: Device base class support
    Adds the foundational implementation of the RDE Device class,
    which represents a Redfish-accessible device managed by the BMC.
    
    Key features:
       - Inherits from xyz.openbmc_project.RDE.Device D-Bus interface
       - Initializes D-Bus properties: Id, Name, NegotiationStatus
       - Internal Metadata struct for non-D-Bus attributes
       - Emits DeviceUpdated signal on property changes
       - Provides getResourceRegistry() accessor
       - Prepares for binds_to_endpoint association support
    
    This class serves as the base for future discovery, negotiation,
    and operational extensions.
    
    Signed-off-by: Jayanth Othayoth <Jayanth.Othayoth@amd.com>

---
+++ rde/device.cpp
@@ -0,0 +1,126 @@
+#include "device.hpp"
+
+#include <iostream>
+
+namespace pldm::rde
+{
+Device::Device(sdbusplus::bus::bus& bus, const std::string& path,
+               pldm::InstanceIdDb* instanceIdDb,
+               pldm::requester::Handler<pldm::requester::Request>* handler,
+               const uint8_t eid, uint8_t tid, const std::string& uuid) :
+    sdbusplus::server::object::object<
+        sdbusplus::xyz::openbmc_project::RDE::server::Device>(
+        bus, path.c_str()),
+    instanceIdDb_(instanceIdDb), handler_(handler), eid_(eid), tid_(tid),
+    uuid_(uuid), currentState_(DeviceState::NotReady)
+{
+    info(
+        "RDE : device Object creating device UUID:{UUID} EID:{EID} Path:{PATH}",
+        "UUID", uuid, "EID", static_cast<int>(eid), "PATH", path);
+
+    this->id(uuid);
+    this->name("Device_" + std::to_string(eid));
+    this->negotiationStatus(NegotiationStatus::NotStarted);
+}
+
+Device::~Device()
+{
+    info("RDE: D-Bus Device object destroyed");
+}
+
+void Device::refreshDeviceInfo()
+{
+    // Placeholder for actual refresh logic
+    std::map<std::string,
+             std::variant<std::string, uint16_t, uint32_t, uint8_t>>
+        changed;
+    changed["Name"] = this->name();
+    // emitDeviceUpdatedSignal(changed);
+}
+
+Metadata& Device::getMetadata()
+{
+    return metaData_;
+}
+
+void Device::setMetadata(const Metadata& meta)
+{
+    metaData_ = meta;
+}
+
+MetadataVariant Device::getMetadataField(const std::string& key) const
+{
+    if (key == "devProviderName")
+        return metaData_.devProviderName;
+    if (key == "etag")
+        return metaData_.etag;
+    if (key == "devConfigSignature")
+        return metaData_.devConfigSignature;
+    if (key == "mcMaxTransferChunkSizeBytes")
+        return metaData_.mcMaxTransferChunkSizeBytes;
+    if (key == "devMaxTransferChunkSizeBytes")
+        return metaData_.devMaxTransferChunkSizeBytes;
+    if (key == "mcConcurrencySupport")
+        return metaData_.mcConcurrencySupport;
+    if (key == "deviceConcurrencySupport")
+        return metaData_.deviceConcurrencySupport;
+    if (key == "protocolVersion")
+        return metaData_.protocolVersion;
+    if (key == "encoding")
+        return metaData_.encoding;
+    if (key == "sessionId")
+        return metaData_.sessionId;
+    if (key == "mcFeatureSupport")
+        return metaData_.mcFeatureSupport;
+    if (key == "devFeatureSupport")
+        return metaData_.devFeatureSupport;
+    if (key == "devCapabilities")
+        return metaData_.devCapabilities;
+    else
+        throw std::invalid_argument("Unknown metadata key: " + key);
+}
+
+void Device::setMetadataField(const std::string& key,
+                              const MetadataVariant& value)
+{
+    if (key == "devProviderName")
+        metaData_.devProviderName = std::get<std::string>(value);
+    else if (key == "etag")
+        metaData_.etag = std::get<std::string>(value);
+    else if (key == "devConfigSignature")
+        metaData_.devConfigSignature = std::get<uint32_t>(value);
+    else if (key == "mcMaxTransferChunkSizeBytes")
+        metaData_.mcMaxTransferChunkSizeBytes = std::get<uint32_t>(value);
+    else if (key == "devMaxTransferChunkSizeBytes")
+        metaData_.devMaxTransferChunkSizeBytes = std::get<uint32_t>(value);
+    else if (key == "deviceConcurrencySupport")
+        metaData_.deviceConcurrencySupport = std::get<uint8_t>(value);
+    else if (key == "mcConcurrencySupport")
+        metaData_.mcConcurrencySupport = std::get<uint8_t>(value);
+    else if (key == "protocolVersion")
+        metaData_.protocolVersion = std::get<std::string>(value);
+    else if (key == "encoding")
+        metaData_.encoding = std::get<std::string>(value);
+    else if (key == "sessionId")
+        metaData_.sessionId = std::get<std::string>(value);
+    else if (key == "mcFeatureSupport")
+        metaData_.mcFeatureSupport = std::get<FeatureSupport>(value);
+    else if (key == "devFeatureSupport")
+        metaData_.devFeatureSupport = std::get<FeatureSupport>(value);
+    else if (key == "devCapabilities")
+        metaData_.devCapabilities = std::get<DeviceCapabilities>(value);
+    else
+        throw std::invalid_argument("Unknown metadata key: " + key);
+}
+
+DeviceState Device::getState() const
+{
+    return currentState_;
+}
+
+void Device::updateState(DeviceState newState)
+{
+    currentState_ = newState;
+}
+
+} // namespace pldm::rde
---
+++ rde/device.hpp
@@ -0,0 +1,146 @@
+#pragma once
+
+#include "device_common.hpp"
+#include "xyz/openbmc_project/RDE/Device/server.hpp"
+
+#include <common/instance_id.hpp>
+#include <requester/handler.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+
+#include <map>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <variant>
+#include <vector>
+
+namespace pldm::rde
+{
+
+/**
+ * @class Device
+ * @brief Represents a Redfish-capable device managed via D-Bus.
+ */
+class Device :
+    public sdbusplus::server::object::object<
+        sdbusplus::xyz::openbmc_project::RDE::server::Device>
+{
+  public:
+    /**
+     * @brief Constructor
+     * @param[in] bus - The D-Bus bus object
+     * @param[in] path - The D-Bus object path
+     * @param[in] instanceIdDb  Pointer to the instance ID database used for
+     *                          PLDM message tracking.
+     * @param[in] handler       Pointer to the PLDM request handler for sending
+     *                          and receiving messages.
+
+     * @param[in] eid -  MCTP Endpoint ID used in PLDM stack
+     * @param[in] tid - Target ID used in PLDM stack.
+     * @param[in] uuid - Internal registration identifier.
+     */
+    Device(sdbusplus::bus::bus& bus, const std::string& path,
+           pldm::InstanceIdDb* instanceIdDb,
+           pldm::requester::Handler<pldm::requester::Request>* handler,
+           const uint8_t eid, uint8_t tid, const std::string& uuid);
+
+    // Defaulted special member functions
+    Device() = delete;
+    Device(const Device&) = delete;
+    Device(Device&&) = delete;
+    Device& operator=(const Device&) = delete;
+    Device& operator=(Device&&) = delete;
+    virtual ~Device();
+
+    /**
+     * @brief Refreshes and updates capability and schema metadata from the
+     * device.
+     */
+    void refreshDeviceInfo() override;
+
+    /**
+     * @brief Access the device metadata.
+     * @return Reference to metadata.
+     */
+    Metadata& getMetadata();
+
+    /**
+     * @brief Set the device metadata.
+     * @param meta Metadata to assign.
+     */
+    void setMetadata(const Metadata& meta);
+
+    /**
+     * @brief Get metadata field by key.
+     *
+     * This function retrieves an individual metadata field using its string
+     * key. Supported types include: std::string, uint8_t, uint16_t, uint32_t,
+     * FeatureSupport, and DeviceCapabilities.
+     *
+     * @param key Metadata field name.
+     * @return Variant holding the value of the metadata field.
+     */
+    MetadataVariant getMetadataField(const std::string& key) const;
+
+    /**
+     * @brief Set metadata field by key.
+     *
+     * This function assigns a value to a metadata field using its string key.
+     * Supported types include: std::string, uint8_t, uint16_t, uint32_t,
+     * FeatureSupport, and DeviceCapabilities.
+     *
+     * @param key Metadata field name.
+     * @param value Variant containing the new value to set.
+     */
+    void setMetadataField(const std::string& key, const MetadataVariant& value);
+
+    /**
+     * @brief Returns reference to the PLDM instance ID database.
+     * @return Reference to pldm::InstanceIdDb
+     */
+    inline pldm::InstanceIdDb& getInstanceIdDb()
+    {
+        return *instanceIdDb_;
+    }
+
+    /**
+     * @brief Returns a pointer to the PLDM request handler.
+     * @return Pointer to Handler of PLDM Request
+     */
+    inline pldm::requester::Handler<pldm::requester::Request>* getHandler()
+    {
+        return handler_;
+    }
+
+    /**
+     * @brief Returns device endpoint identifier (EID).
+     * @return 8-bit unsigned integer EID value
+     */
+    inline uint8_t getEid() const
+    {
+        return eid_;
+    }
+
+    /**
+     * @brief Attempts to update the device state.
+     * @param newState The desired DeviceState.
+     */
+    void updateState(DeviceState newState);
+
+    /**
+     * @brief Returns the current device state.
+     */
+    DeviceState getState() const;
+
+  private:
+    Metadata metaData_;
+    pldm::InstanceIdDb* instanceIdDb_ = nullptr;
+    pldm::requester::Handler<pldm::requester::Request>* handler_ = nullptr;
+    uint8_t eid_;
+    [[maybe_unused]] uint8_t tid_;
+    std::string uuid_;
+    DeviceState currentState_;
+};
+
+} // namespace pldm::rde
