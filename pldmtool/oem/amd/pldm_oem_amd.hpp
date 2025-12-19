beb7604fa7b5336c02dddfdbf1abecd5d0d158df
spargaon <Shirish.Pargaonkar@amd.com>
2025-12-19 10:26:58 -0600

    pldmtool: File I/O & checksum options for amdMctpSfs
    Introduce new subcommand `amdMctpSfs` with options to read
    request data from a file (`--file-in`), write response
    data to a file (`--file-out`), and append or validate
    checksum (`--checksum`).
    These additions improve usability for large payloads and
    ensure data integrity during MCTP SFS transactions.
    
    Contents of the file specified in option --file-in are
    appended to the contents of the option --data.
    
    Signed-off-by: Shirish Pargaonkar <Shirish.Pargaonkar@amd.com>

---
+++ pldmtool/oem/amd/pldm_oem_amd.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <CLI/CLI.hpp>
+
+namespace pldmtool
+{
+namespace oem_amd
+{
+
+void registerCommand(CLI::App& app);
+
+} // namespace oem_amd
+} // namespace pldmtool
--- pldmtool/pldm_cmd_helper.hpp
+++ pldmtool/pldm_cmd_helper.hpp
@@ -30,6 +30,7 @@ namespace pldmtool
 namespace helper
 {
 
+constexpr uint8_t POLL_INTERVAL = 5;
 constexpr uint8_t PLDM_ENTITY_ID = 8;
 using ordered_json = nlohmann::ordered_json;
 
@@ -116,7 +117,8 @@ class CommandInterface
     virtual void exec();
 
     int pldmSendRecv(std::vector<uint8_t>& requestMsg,
-                     std::vector<uint8_t>& responseMsg);
+                     std::vector<uint8_t>& responseMsg,
+                     uint16_t pollInterval = POLL_INTERVAL);
 
     /**
      * @brief get MCTP endpoint ID
@@ -160,6 +162,7 @@ class CommandInterface
     uint8_t numRetries = 0;
     bool mctpPreAllocTag = false;
     uint8_t mctpNetworkId = 1;
+    uint16_t pollInterval = POLL_INTERVAL;
 };
 
 } // namespace helper
