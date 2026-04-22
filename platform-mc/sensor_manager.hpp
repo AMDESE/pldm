ca6de4e402c43c389f90f76de0363ad9c3199f2a
Shirish Pargaonkar <Shirish.Pargaonkar@amd.com>
2026-04-22 16:57:25 -0500

    pldm: Verbose pldmd sensor and effecter log messages
    Log sensor read or parse errors only if pldmd is
    running in verbose mode.
    
    Signed-off-by: Shirish Pargaonkar <Shirish.Pargaonkar@amd.com>

--- platform-mc/sensor_manager.hpp
+++ platform-mc/sensor_manager.hpp
@@ -40,7 +40,8 @@ class SensorManager
 
     explicit SensorManager(sdeventplus::Event& event,
                            TerminusManager& terminusManager,
-                           TerminiMapper& termini, Manager* manager);
+                           TerminiMapper& termini, Manager* manager,
+                           const bool verbose);
 
     /** @brief starting sensor polling task
      */
@@ -124,6 +125,9 @@ class SensorManager
 
     /** @brief pointer to Manager */
     Manager* manager;
+
+    /** @brief Enable verbose logging for debug purposes */
+    bool verbose{false};
 };
 } // namespace platform_mc
 } // namespace pldm
