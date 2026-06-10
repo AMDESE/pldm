88bc044799d1600c0e5092b441925bd8c80d55f2
Shirish Pargaonkar <Shirish.Pargaonkar@amd.com>
2026-06-10 11:22:12 -0500

    PldmMessagePollEvent: Emit correct event Id
    When pldmd emits a D-Bus signal for PldmMessagePollEvent,
    it emits eventId along with other parameters.
    Fix the incorrect eventId that gets emmited.
    
    For example, emit UINT16 19457: instead of UINT16 0:
    
    Signed-off-by: Shirish Pargaonkar <Shirish.Pargaonkar@amd.com>

--- platform-mc/event_manager.cpp
+++ platform-mc/event_manager.cpp
@@ -816,7 +816,7 @@ exec::task<int> EventManager::pollForPlatformEventTask(
                                         polledEventId, eventMessage);
 #ifdef OEM_AMD
                 handlePollEventData(polledEventTid, formatVersion,
-                                    polledEventClass, eventId,
+                                    polledEventClass, polledEventId,
                                     dataTransferHandles, eventDataSizes,
                                     eventMessage);
 #endif
