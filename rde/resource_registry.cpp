e600cb8c365f75a21788b692f6133a3ebb33eeda
Konstantin Aladyshev <aladyshev22@gmail.com>
2026-06-29 10:41:58 +0300

    rde: Drop workaround for duplicate URI segments
    Currently the constructFullUriRecursive contains a workaround for the
    duplicate segments in the URI. Apparently this workaround was
    introduced to mask a bug with additional resources that earlier had
    their parent set to ResourceID instead of the ContainingResourceID.
    It is worth to notice that this workaround wasn't working for the cases
    when there are more than one additional resources in the PDR.
    For example it produced URIs:
    - NetworkDeviceFunctions/0
    - NetworkDeviceFunctions/0/1
    - NetworkDeviceFunctions/0/2
    - NetworkDeviceFunctions/0/3
    instead of
    - NetworkDeviceFunctions/0
    - NetworkDeviceFunctions/1
    - NetworkDeviceFunctions/2
    - NetworkDeviceFunctions/3
    In any case, since the bug with additional resources parent was fixed,
    the workaround code can be dropped.
    
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- rde/resource_registry.cpp
+++ rde/resource_registry.cpp
@@ -238,41 +238,7 @@ std::string ResourceRegistry::constructFullUriRecursive(
     if (!parentUri.empty() && parentUri.back() != '/')
         parentUri += "/";
 
-    // TODO: Workaround for RDE PDRs that redundantly encode segments like
-    // "0/0/Settings" If parent URI ends with the same segment as the beginning
-    // of `part`, skip duplication
-    std::vector<std::string> parentSegments;
-    std::stringstream ss(parentUri);
-    std::string segment;
-    while (std::getline(ss, segment, '/'))
-    {
-        if (!segment.empty())
-            parentSegments.push_back(segment);
-    }
-
-    std::vector<std::string> partSegments;
-    std::stringstream ps(part);
-    while (std::getline(ps, segment, '/'))
-    {
-        if (!segment.empty())
-            partSegments.push_back(segment);
-    }
-
-    if (!parentSegments.empty() && !partSegments.empty() &&
-        parentSegments.back() == partSegments.front())
-    {
-        // Skip the first segment of `part` to avoid duplication
-        partSegments.erase(partSegments.begin());
-    }
-
-    for (const auto& seg : partSegments)
-    {
-        parentUri += seg + "/";
-    }
-
-    // Remove trailing slash if added
-    if (!parentUri.empty() && parentUri.back() == '/')
-        parentUri.pop_back();
+    parentUri += part;
 
     return parentUri;
 }
